7a21305f3e0022f27002191a6c3da28f7f14f545
Carmelytta <carmen.martin.bejarano@gmail.com>
2016-08-12 16:57:16 +0000

    Guardo la humedadT en la bbdd la medidad del sensor de temperatura

--- sensor-humedad.py
+++ sensor-humedad.py
@@ -1,42 +1,70 @@
 #!/usr/bin/python
-# -*- coding: utf-8 -*-
 
-#EJEMPLO DE BLINKING CON RASPBERRY PI
-#Escrito por Gl4r3
-import RPi.GPIO as GPIO 					#importamos la libreria y cambiamos su nombre por "GPIO"
-import time 								#necesario para los delays
+############################################
+#
+#	   TFG : SISTEMA DE RIEGO 
+#
+############################################
 
-#establecemos el sistema de numeracion que queramos, en mi caso BCM
-GPIO.setmode(GPIO.BCM)
 
-#configuramos los pines GPIO
-#GPIO.setup(17, GPIO.OUT)
+# LIBRERIAS
+import sys
+import Adafruit_DHT
+import time
+import sqlite3
+import RPi.GPIO as GPIO
 
-muestra = GPIO.setup(4, INPUT)			#Entrada digital del sensor
+# PARAMETROS EN LINEA DE COMANDOS
+sensor_args = { '11': Adafruit_DHT.DHT11,
+                '22': Adafruit_DHT.DHT22,
+                '2302': Adafruit_DHT.AM2302 }
+#definicion de la funcion
+def humedadT(hum):
+   conn = sqlite3.connect('/home/pi/bbdd/riego.db')
+   cursor = conn.cursor()
+   cursor.execute("INSERT INTO humedadT values(date('now'), time('now'), (?))", (hum,))
+   conn.commit()
+   conn.close()
+# VARIABLES
 
-bajo = GPIO.input(4, GPIO.LOW)
-alto = GPIO.input(4, GPIO.HIGH)
+s = []          #Lista de 20 posiciones para dibujar
 
-#Comprobamos en que modo estamos
 
-if muestra == alto:
-	print "No hay humedad"
-else: 
-	print "Hay humedad"
+#for row in cursor.execute("SELECT * FROM tempertatura"):
+ #   print row
+# MODO BCM DE LA RASPBERRY PI
+GPIO.setmode(GPIO.BCM)   
+# DEFINIMOS EL PIN 17 COMO ENTRADA
+GPIO.setup(17,GPIO.IN)    
+# Warnings
+GPIO.setwarnings(False)
 
-###############################################
 
-#Salida analogica
+# PROGRAMA
 
-##############################################
+# Comprobamos que el numero de parametros es el correcto
 
-#if muestra >= 1000:
-#	print "Sensor desconectado"
-#elif muestra < 1000 && muestra >= 600:
-#	print "Suelo seco"
-#elif muestra < 600 && muestra >= 370:
-#	print "Suelo humedo"
-#elif muestra < 370:
-#	print "Sensor en agua"
-#else:
-#	print "Fuera de rango"
+if len(sys.argv) == 3 and sys.argv[1] in sensor_args:
+    sensor = sensor_args[sys.argv[1]]
+    pin = sys.argv[2] #Escogemos el pin 4 como entrada del sensor DHT
+    #count = int(sys.argv[3]) #Bucle de captura del sensor
+else:
+    print('usage: sudo ./Adafruit_DHT.py [11|22|2302] GPIOpin#')
+    print('example: sudo ./Adafruit_DHT.py 2302 4 - Read from an AM2302 connected ')
+    sys.exit(1)
+
+
+
+humidity, temperature = Adafruit_DHT.read_retry(sensor, pin)
+
+#print('Temperatura={0:0.1f}*  Humedad del aire= {1:0.1f}%'.format(temperature, humidity))
+
+humedadT(humidity)
+if humidity is not None and temperature is not None:
+    #while count > 0:
+    print('Temp={0:0.1f}*  Humidity={1:0.1f}%'.format(temperature, humidity))
+     #  count = count - 1
+    
+else:
+    print('Failed to get reading. Try again!')
+    sys.exit(1)
